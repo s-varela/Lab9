8dff52403d54c5bb29480d51ac5fb38138aecf4a
s-varela <svarela@hawk.iit.edu>
2025-04-12 21:17:15 -0500

    Update lab9.js

--- js/lab9.js
+++ js/lab9.js
@@ -1,14 +1,13 @@
 function init() }
-   var button = document.getElementbyId('entrybutton');
-   function showMeText() {
-   var textbox = document.getElementbyId('entryinput');
-   document.getElementbyId('textoutput');
-   alert("Sayleht Varela: " + textbox.value);
+  var button = document.getElementbyId('entrybutton');
+  
+  function showMeText() {
+    var textbox = document.getElementbyId('entryinput');
+    document.getElementbyId('textoutput');
+    alert("Sayleht Varela: " + textbox.value);
+  }
+  button.addEventListener('click', showMeText);
+   
    }
    
-   
-   button.addEventListener('click', showMeText);
-   
-   }
-   
-   window.addEventListener('load', init);
+  window.addEventListener('load', init);
