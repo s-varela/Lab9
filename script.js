3668080e421b87c5ba725bf94735474b1e908dbc
s-varela <svarela@hawk.iit.edu>
2025-04-12 21:54:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function init()}
-  var button = document.getElementbyId('entrybutton');
-  
-  function showMeText(){
-    var textbox = document.getElementbyId('entryinput');
-    document.getElementbyId('textoutput').innerHTML = textbox.value;
-    alert("Sayleht Varela: " + textbox.value);
-   }
+function init(){
+  var button = document.getElementById('entrybutton');
 
-  button.addEventListener('click', showMeText); 
+  function showMeText(){
+    var textbox = document.getElementById('entryinput');
+    document.getElementById('textoutput').innerHTML = textbox.value;
+    alert("Sayleht Varela: " + textbox.value);
   }
-   
+
+  button.addEventListener('click', showMeText);
+  }
+  
 window.addEventListener('load', init);
